1484b9abede91ef1363b77b72861db0863dcaf40
vexa256@gmail.com <vexa256@gmail.com>
2023-01-07 11:33:29 +0000

    first commit

---
+++ front/about/About.blade.php
@@ -0,0 +1,64 @@
+<section class="ls ms s-py-xl-150 s-py-lg-130 s-py-md-90 s-py-60 c-gutter-0">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 offset-small-left">
+                <div class="img-wrap">
+                    <img src="res/About.png" alt="img">
+                </div>
+            </div>
+            <div class="col-lg-6 text-center text-sm-left">
+                <div class="fw-divider-space divider-40 divider-lg-0"></div>
+                <p class="special-heading color-darkgrey sub-title">
+                    <span class="text-uppercase">
+                        AfriChild
+                    </span>
+                </p>
+                <h4 class="special-heading ">
+                    <span class="text-capitalize ">
+                        All About the African Child
+                    </span>
+                    </h2>
+                    <div class="fw-divider-space divider-30 divider-lg-55"></div>
+                    <p>
+
+                        <span class="fw-bolder">
+                            <div>
+                                <h3>Mission
+                                </h3>
+
+                                <p>The AfriChild Centre exists to generate research that informs policy and practice for
+                                    the wellbeing of children.</p>
+                            </div>
+                        </span>
+
+                        <br>
+                        {{-- <br> --}}
+
+
+                        <span class="fw-bolder">
+                            <div>
+                                <h3>Vision
+                                </h3>
+
+                                <p>We strive for an Africa where children's wellbeing is realised for sustainable
+                                    development.</p>
+                            </div>
+                        </span>
+
+                    <blockquote class="pt-5">
+                        “We are a Centre of Excellence in the study of the African child.”
+
+
+                    </blockquote>
+
+
+
+
+                    </p>
+
+                    <div class="fw-divider-space divider-30 divider-lg-60"></div>
+
+            </div>
+        </div>
+    </div>
+</section>
---
+++ front/slider/slides.blade.php
@@ -0,0 +1,34 @@
+<li class="ds slide-1 cover-image s-overlay text-left  ">
+    <img style="height: 80vh;" src="{{ asset('res/bg1.jpg') }}" alt="img">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 text-center text-sm-left">
+                <div class="intro_layers_wrapper">
+                    <div class="intro_layers d-flex flex-column flex-md-row align-items-md-center">
+
+                        {{-- <div class="intro_layer" data-animation="fadeIn">
+                            <img src="{{ asset('images/chess.png') }}" alt="">
+                        </div> --}}
+                        <div class="intro_layer" data-animation="fadeInLeft" data-delay="150">
+                            <h4 class="text-uppercase thin">Child Focused <span
+                                    class="color-main text-capitalize bold">Research</span>
+                            </h4>
+                            <p>
+                                We are a Centre of Excellence in the study of the African child
+                            </p>
+                        </div>
+
+
+                    </div> <!-- eof .intro_layers -->
+                    <div class="intro_layers ">
+                        <div class="many-buttons intro_layer" data-animation="fadeIn" data-delay="150">
+                            {{-- <a href="#" class="btn btn-maincolor medium-btn">start now</a> --}}
+                            <a href="{{ route('AboutTheAfriChildCenterMakerere') }}"
+                                class="btn btn-outline-maincolor medium-btn">learn more</a>
+                        </div>
+                    </div>
+                </div> <!-- eof .intro_layers_wrapper -->
+            </div> <!-- eof .col-* -->
+        </div><!-- eof .row -->
+    </div><!-- eof .container-fluid -->
+</li>
